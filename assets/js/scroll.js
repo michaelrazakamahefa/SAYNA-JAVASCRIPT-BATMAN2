db213b4c5430da672d3f8bd06ebea11024e13147
michael <michael.razakamahefa@gmail.com>
2023-06-09 08:23:52 +0200

    correction du projet

--- assets/js/scroll.js
+++
@@ -1,9 +0,0 @@
-window.onload = () => {
-    let arrow = document.getElementsByClassName('arrow2 hoveryellow');
-    arrow[0].addEventListener('click', () => {
-        window.scrollTo(top, 'smooth');
-    });
-    arrow[1].addEventListener('click', () => {
-        window.scrollBy(0, window.innerHeight);
-    });
-};
